9568e5e4fdbd77cd8db813ae0b38d67572adf242
hrjray <1943724980@qq.com>
2022-02-08 23:39:26 +0800

    初始化环境数组

--- kern/pmap.c
+++ kern/pmap.c
@@ -170,7 +170,6 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
-	struct Env* envs=NULL;
 	envs=(struct Env*)boot_alloc(NENV*sizeof(struct Env));
 	memset(envs,0,NENV*sizeof(struct Env));
 	//////////////////////////////////////////////////////////////////////
@@ -203,7 +202,7 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
-	boot_map_region(kern_pgdir,UENVS,NENV*sizeof(struct Env),PADDR(envs),PTE_U);
+	boot_map_region(kern_pgdir,UENVS,PTSIZE,PADDR(envs),PTE_U);
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
 	// stack.  The kernel stack grows down from virtual address KSTACKTOP.
